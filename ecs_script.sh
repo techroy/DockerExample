4b8a376099d089304c1969dcd5163d778eb68748
techroy <ramkrishnaroy20@gmail.com>
2024-08-24 18:54:28 +0530

    changed ecs script

--- ecs_script.sh
+++ ecs_script.sh
@@ -4,9 +4,11 @@ sed -i "s#TASK_DEF_LOG_GROUP_NAME#$TASK_DEFINITION_NAME#g" task-definition.json
 sed -i "s#REGION_NAME#$AWS_DEFAULT_REGION#g" task-definition.json
 sed -i "s#FAMILY_NAME#$TASK_DEFINITION_NAME#g" task-definition.json
 
+cat task-definition.json
+echo "sed is done"
+aws ecs register-task-definition --cli-input-json file://task-definition.json --region="${AWS_DEFAULT_REGION}"
 
-aws ecs register-task-definition --cli-input-json file://task-definition.json
-
+echo "ecs register works"
 REVISION=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.revision`
 echo "REVISION= " "${REVISION}"
 aws ecs update-service --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
