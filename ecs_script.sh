83b05df68ebf0e13c0442dee1b75e937b36cb51b
techroy <ramkrishnaroy20@gmail.com>
2024-09-03 10:40:06 +0530

    added create new service for jenkins

--- ecs_script.sh
+++ ecs_script.sh
@@ -10,7 +10,23 @@ echo "sed is done"
 aws ecs register-task-definition --cli-input-json file://task-definition.json --region="${AWS_DEFAULT_REGION}"
 
 echo "ecs register works"
+
+#Get latest revision
 REVISION=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.revision`
 echo "REVISION= " "${REVISION}"
-echo "Service_Name= " "${SERVICE_NAME}"
-aws ecs update-service --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
+
+SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER_NAME} --region ${AWS_DEFAULT_REGION} | jq .failures[]`
+
+#Create or update service
+
+if [ "$SERVICES" == "" ]; then
+    echo "entered existing service"
+    DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER_NAME} --region ${AWS_DEFAULT_REGION} | jq .services[].desiredCount`
+    if [ ${DESIRED_COUNT} = "0" ]; then
+        DESIRED_COUNT="1"
+    fi
+    aws ecs update-service --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
+else
+    echo "entered new service"
+    aws ecs create-service --service-name ${SERVICE_NAME} --desired-count "${DESIRED_COUNT}" --task-definition ${TASK_DEFINITION_NAME} --cluster ${CLUSTER_NAME} --region ${AWS_DEFAULT_REGION}    
+fi    
